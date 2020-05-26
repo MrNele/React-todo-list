297060b6d6e24248741dc953226999cc1be4b57f
MrNele <nemanja1225@gmail.com>
2020-05-26 13:35:16 +0200

    first commit

---
+++ src/components/TodoInputs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TodoInputs extends Component {
+    render() {
+        return (
+            <div>
+                hello from Todo item
+            </div>
+        )
+    }
+}
