da52a8918b9c358c306e0809f8b484d76ce01661
MrNele <nemanja1225@gmail.com>
2020-05-27 15:01:25 +0200

    Update and rename TodoInputs.js to TodoInput.js

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class TodoInputs extends Component {
+export default class TodoInput extends Component {
     render() {
         return (
             <div>
